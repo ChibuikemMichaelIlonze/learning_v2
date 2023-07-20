da715fc0b547c1b3e3b4216360bf10007e1e3ae9
Chibuikem Michael Ilonze <56983788+ChibuikemMichaelIlonze@users.noreply.github.com>
2023-07-20 14:16:18 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Heading</h1>
+    <ol>
+      <li>A</li>
+      <li>B</li>
+      <li>C</li>
+    </ol>
+  </div>,
+  document.getElementById("root")
+);
